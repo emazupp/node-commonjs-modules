901b5a8917087d51239b095c2b18c8c08b26f8a4
emazupp <zuppardo.emanuele2001@gmail.com>
2024-11-18 15:18:35 +0100

    created names.js

---
+++ namer.js
@@ -0,0 +1,1 @@
+const fullname = (firstName, secondName) => ({ firstName, secondName });
---
+++ people.js
@@ -0,0 +1,15 @@
+/* Lavoriamo con i moduli commonjs esportando e importando valori e funzioni. Segui questi tre step creando i rispettivi files:
+names.js
+    Crea una funzione che accetta due parametri: firstName, lastName.
+    La funzione restituisce un oggetto con le proprietà firstName e lastName.
+    Esporta la funzione dal file.
+hobbies.js
+    Crea una funzione che accetta tre parametri: hobbyOne, hobbyTwo, hobbyThree.
+    La funzione restituisce un oggetto con una proprietà hobbies, che è un array di hobby.
+    Esporta la funzione dal file.
+people.js
+    Importa la tua funzione da names.js
+    Importa la tua funzione da hobbies.js
+    Crea una funzione che non ha parametri.
+    All'interno della tua funzione, usa le tue due funzioni precedenti per costruire l'oggetto.
+    La funzione dovrebbe restituisce un oggetto con tre proprietà: firstName, lastName, hobbies. */
